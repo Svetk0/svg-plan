46bc30bf20818629e58053e1d29538487ec2182b
svetk0 <sv.solomatnikova@gmail.com>
2024-06-20 19:57:54 +0300

    trying add bufer in array

--- index.js
+++ index.js
@@ -24,7 +24,34 @@ const data = [
         "angle": 270,
     },
 ];
+const getCoordinateX = (length, angle) => { 
+    // Преобразование входных данных
+    const convertToRadians = (angleDeg) => { 
+        const pi = Math.PI;
+        return (angleDeg * pi / 180)
+    }
 
+ // Вычисление координат
+    const angleRad = convertToRadians(angle);
+    const x =  Math.round (length/10 * Math.cos(angleRad));
+    //const y = Math.round (length/10 * Math.sin(angleRad));
+    //return [Math.abs(x), Math.abs(y)];
+    return x;
+}
+const getCoordinateY = (length, angle) => {
+    // Преобразование входных данных
+    const convertToRadians = (angleDeg) => {
+        const pi = Math.PI;
+        return (angleDeg * pi / 180)
+    }
+
+    // Вычисление координат
+    const angleRad = convertToRadians(angle);
+    //const x = Math.round(length / 10 * Math.cos(angleRad));
+    const y = Math.round (length/10 * Math.sin(angleRad));
+    //return [Math.abs(x), Math.abs(y)];
+    return y;
+}
 
 const getCoordinates = (length, angle) => { 
     // Преобразование входных данных
@@ -37,29 +64,45 @@ const getCoordinates = (length, angle) => {
     const angleRad = convertToRadians(angle);
     const x =  Math.round (length/10 * Math.cos(angleRad));
     const y = Math.round (length/10 * Math.sin(angleRad));
-    return [Math.abs(x), Math.abs(y)];
+    //return [Math.abs(x), Math.abs(y)];
+    return [x,y];
 }
 
 
 const getPath = () => { 
-    const initial = [0,0];
+    const initial = [222,224];
     const pointsStr = data.map((point, index) => { 
-        // const x = getCoordinateX(point.length, point.angle);
-        // const y = getCoordinateY(point.length, point.angle);
-        point[index] = getCoordinates(point.length, point.angle);;
+        const x = getCoordinateX(point.length, point.angle);
+        const y = getCoordinateY(point.length, point.angle);
+        //point[index] = getCoordinates(point.length, point.angle);
+        point[index] = [x, y];
+     
         console.log(`point[${index}]: ${point[index]} `);
         //return parseFloat(point[index].join(' '), 10);
         return point[index];
     })
-
+    let pointsWithoutGap = [ ...pointsStr.reverse()];
+    //let pointsRev = [...pointsStr.reverse()]
+    let copy = [...pointsWithoutGap];
+    //let copy = [...pointsWithoutGap];
+        //pointsWithoutGap = pointsStr.concat.apply(initial, pointsStr);
     let merged = [];
-    merged = merged.concat.apply(merged, pointsStr);
+    merged = merged.concat.apply(merged, pointsWithoutGap);
+    //let copy = [...merged];
     //const pointsAdd = merged.concat.apply(pointsStr,initial);
-    const pointsStart = merged.concat.apply(initial, pointsStr);
-    const absolutePath = merged.map((absPoint, index) => { 
-        return absPoint + pointsStart[index];
+    let bufer = [...initial];
+    console.log('copy', copy);
+    console.log('merged', merged);
+    //const pointsStart = merged.concat.apply(initial, pointsStr);
+    const absolutePath = pointsWithoutGap.map((absPoint, index) => { 
+        pointsWithoutGap.splice(0,bufer);
+        absX = absPoint[0] + copy[index][0];
+        absY = absPoint[1] + copy[index][1];
+        absPoint[index] = [absX, absY];
+        bufer = [absX, absY];
+        console.log(absX, absY);
+        return [absX, absY];
     })
-
 
     console.log('map', absolutePath);
     return absolutePath.join(' ');
